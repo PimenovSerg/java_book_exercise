bf9514227f15bebb49ab1b8c48fa7b7e67b03ad8
PimenovSerg <108987794+PimenovSerg@users.noreply.github.com>
2024-06-15 21:24:39 +0300

    add BoolDemo

---
+++ Chapter2/BoolDemo.java
@@ -0,0 +1,17 @@
+class BoolDemo {
+    public static void main(String[] args) {
+        boolean b;
+
+        b = false;
+        System.out.println("Значние b: " + b);
+        b = true;
+        System.out.println("Значние b: " + b);
+
+        if(b) System.out.println("Эта инструкция выполняется");
+
+        b = false;
+        if(b) System.out.println("Эта инструкция не выполняется");
+
+        System.out.println("Результат сравенния 10 > 9: " + (10 > 9));
+    }
+}
