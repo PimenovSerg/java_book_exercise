dd6b2c5e8f878c0835d9a65ec63f30899ec7b9c5
PimenovSerg <108987794+PimenovSerg@users.noreply.github.com>
2024-06-15 21:17:42 +0300

    add pim

---
+++ Chapter2/CharArithDemo.java
@@ -0,0 +1,17 @@
+class CharArithDemo {
+    public static void main(String[] args) {
+        char ch;
+
+        ch = 'X';
+
+        System.out.println("сh содержит: " + ch);
+
+        ch++;
+
+        System.out.println("теперь ch содержит: " + ch);
+
+        ch = 90;
+
+        System.out.println("теперь ch содержит: " + ch);
+    }
+}
